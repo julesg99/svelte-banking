8ce53a6e0b023a74a094bf888f95fb626f7d87d8
Julian Garver <julian.garver@integrityinspired.com>
2023-03-16 16:23:34 -0500

    fixed filter display

--- src/components/transactions/TransactionsFilters.svelte
+++ src/components/transactions/TransactionsFilters.svelte
@@ -4,7 +4,7 @@
   export let filters: transactionFilters
   export let statusTypes: string[]
 
-  let statusFilter: string = ''
+  let statusFilter: string = 'undefined'
   let categoryFilter: string = ''
   let dateFilter: Date
 
@@ -20,7 +20,7 @@
   <select bind:value={statusFilter} 
     class="h-10 mx-3 p-2 rounded-lg outline outline-1 outline-gray-300 bg-gray-50 shadow-md capitalize"
   >
-    <option selected>Status Filter</option>
+    <option value={'undefined'} selected>Status Filter</option>
     {#each statusTypes as status, index}
       <option value={status} class="capitalize">{status}</option>
     {/each}
@@ -31,7 +31,7 @@
   <input bind:value={categoryFilter} 
     class="h-10 p-2 mx-3 rounded-lg outline outline-1 outline-gray-300 bg-gray-50 shadow-md" placeholder="Filter by Category" 
   />
-  <button on:click={() => {categoryFilter = ''; statusFilter = ''; dateFilter = new Date('999-01-01')}}
+  <button on:click={() => {categoryFilter = ''; statusFilter = 'undefined'; dateFilter = new Date('999-01-01')}}
     class="hover:text-red-500 hover:bg-gray-200 hover:shadow-md hover:outline outline-1 outline-red-500 rounded-lg p-1"
   >
     Clear Filters
